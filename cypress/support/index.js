436fad3c156f77ec1f966e2ea866b4b0d8673650
Xvezda <xvezda@naver.com>
2022-03-17 16:57:12 +0900

    feat: 기본 플레이스홀더 stub 사용

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,4 +21,24 @@ import './commands'
 
 beforeEach(() => {
   cy.fixCypressSpec();
+  cy.intercept(/https?:\/\/thumb\.zumst\.com\/.*/,
+    {
+      middleware: true
+    },
+    req => {
+      const url = new URL(req.url);
+      const pathRegex = /^\/(\d+)x(\d+)\//;
+      if (pathRegex.test(url.pathname)) {
+        const [, w, h] = url.pathname.match(pathRegex);
+        // 사진을 크기에 따라 동일한 플레이스홀더로 치환
+        if (w !== '0' && h !== '0') {
+          req.redirect(`https://picsum.photos/id/56/${w}/${h}`);
+        } else {
+          req.redirect(`https://picsum.photos/id/56/${w === '0' ? h : w}`);
+        }
+      } else {
+        req.destroy();
+      }
+    }
+  );
 });
