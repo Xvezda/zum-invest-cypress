f09dde44ab72fc591ce9f639236f53f2669c3a31
Xvezda <xvezda@naver.com>
2022-03-25 16:14:08 +0900

    test: 해외증시 투표기능 테스트 추가

--- cypress/integration/global_spec.js
+++ cypress/integration/global_spec.js
@@ -10,4 +10,34 @@ describe('해외증시', () => {
   it('스크롤을 하여 해외 실시간 뉴스를 불러온다.', () => {
     cy.shouldRequestOnScroll('@apiRealTimeNews');
   });
+
+  it('로그인하고 투표를 눌러 투표할 수 있다.', () => {
+    cy.intercept('/api/overseas/debates/*/vote', {fixture: 'overseas-debates-vote'})
+      .as('debatesVote');
+
+    cy.wrap([
+        {
+          selector: '.rise',
+          expectTo: 'be.true',
+        },
+        {
+          selector: '.fall',
+          expectTo: 'be.false',
+        }
+      ])
+      .each(({ selector, expectTo }) => {
+        cy.login();
+
+        cy.get('.debate_wrap')
+          .find(selector)
+          .click();
+        
+        cy.wait('@debatesVote')
+          .its('request.body.status')
+          .should(expectTo);
+
+        cy.logout();
+        cy.clearCookies();
+      });
+  });
 });
