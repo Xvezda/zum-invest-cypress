1d52baa9bde30177bda837e23e226dc7a05079ce
Xvezda <xvezda@naver.com>
2022-03-28 11:41:34 +0900

    test: 해외증시 테스트 추가

--- cypress/integration/global_spec.js
+++ cypress/integration/global_spec.js
@@ -1,14 +1,38 @@
 describe('해외증시', () => {
   beforeEach(() => {
-    cy.ignoreKnownError(`Cannot read properties of undefined (reading 'dow')`);
+    cy.ignoreKnownError(/Cannot read properties of undefined \(reading '(dow|children)'\)/);
     cy.stubThirdParty();
     cy.stubInvestApi();
 
-    cy.visit('/global');
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage');
+      }
+    });
+
+    cy.get('.gnb_finance a:contains("해외증시")')
+      .click();
+    cy.wait(['@apiOverseasHome', '@apiOverseasCommon']);
   });
 
-  it('스크롤을 하여 해외 실시간 뉴스를 불러온다.', () => {
-    cy.shouldRequestOnScroll('@apiRealTimeNews');
+  it('다우산업 화살표를 클릭해 주요뉴스를 살펴볼 수 있다.', () => {
+    cy.get('.main_news_list')
+      .scrollIntoView();
+
+    cy.get('.main_news_list + .navi > .next')
+      .click();
+
+    cy.get('.main_news_list ul > li')
+      .as('newsItems')
+      .last()
+      .should('be.visible');
+
+    cy.get('.main_news_list + .navi > .prev')
+      .click();
+
+    cy.get('@newsItems')
+      .first()
+      .should('be.visible');
   });
 
   it('로그인하고 투표를 눌러 투표할 수 있다.', () => {
@@ -26,18 +50,21 @@ describe('해외증시', () => {
         }
       ])
       .each(({ selector, expectTo }) => {
-        cy.login();
-
-        cy.get('.debate_wrap')
+        cy.login()
+          .get('.debate_wrap')
           .find(selector)
-          .click();
-        
-        cy.wait('@debatesVote')
+          .click()
+          .end()
+          .wait('@debatesVote')
           .its('request.body.status')
-          .should(expectTo);
-
-        cy.logout();
-        cy.clearCookies();
+          .should(expectTo)
+          .end()
+          .logout()
+          .clearCookies();
       });
   });
-});
+
+  it('스크롤을 하여 해외 실시간 뉴스를 불러온다.', () => {
+    cy.shouldRequestOnScroll('@apiRealTimeNews');
+  });
+});  // END: 해외증시
