ff16b249f3a7a4f3b62b0ad1893d23a532e01d33
Xvezda <xvezda@naver.com>
2022-03-28 13:13:18 +0900

    test: 해외 주요지수 테스트 추가

--- cypress/integration/global_spec.js
+++ cypress/integration/global_spec.js
@@ -36,6 +36,34 @@ describe('해외증시', () => {
       .should('be.visible');
   });
 
+  describe('해외 주요지수', () => {
+    it('주요국 지수 현황과 주요 지표 현황을 눌러 활성화 할 수 있고 해당 내용을 보여준다.', () => {
+      cy.fixture('overseas-home')
+        .then(home => {
+          cy.get('.major_index')
+            .as('majorIndex')
+            .contains('주요 지표 현황')
+            .click();
+
+          const mainIndexNames = home.indicatorOfMainIndex
+            .map(({ name }) => name);
+
+          cy.wrap(mainIndexNames)
+            .each(name => cy.get('@majorIndex').should('contain', name));
+
+          cy.get('@majorIndex')
+            .contains('주요국 지수 현황')
+            .click();
+
+          const mainCountryNames = home.countryIndexOfMainIndex
+            .map(({ name }) => name);
+
+          cy.wrap(mainCountryNames)
+            .each(name => cy.get('@majorIndex').should('contain', name));
+        });
+    });
+  });  // END: 해외 주요지수
+
   it('로그인하고 투표를 눌러 투표할 수 있다.', () => {
     cy.intercept('/api/overseas/debates/*/vote', {fixture: 'overseas-debates-vote'})
       .as('debatesVote');
