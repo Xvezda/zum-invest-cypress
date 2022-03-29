fd25240c593ed9b3f4a74d463d060c5c600bae12
Xvezda <xvezda@naver.com>
2022-03-29 10:06:15 +0900

    test: 76x48 사이즈 fixture 추가

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -30,6 +30,8 @@ beforeEach(() => {
       req.reply({fixture: '182x77.jpg'});
     } else if (url.pathname.startsWith('/166x116')) {
       req.reply({fixture: '166x116.jpg'});
+    } else if (url.pathname.startsWith('/76x48')) {
+      req.reply({fixture: '76x48.jpg'});
     } else if (url.pathname.startsWith('/54x34')) {
       req.reply({fixture: '54x34.jpg'});
     } else {
