1a3f6c8bd70dfad362e47c9eaec117692b7d8334
Xvezda <xvezda@naver.com>
2022-04-01 14:18:30 +0900

    test: 투자 필진 요청 중복 처리

--- cypress/integration/investment_spec.js
+++ cypress/integration/investment_spec.js
@@ -1,12 +1,17 @@
+const { recurse } = require('cypress-recurse');
+
 describe('투자노트', () => {
   beforeEach(() => {
     // TODO: 원인조사
     cy.ignoreKnownError("Cannot read properties of null (reading 'postMessage')");
+    cy.ignoreKnownError('Network Error');
 
     cy.stubInvestApi();
 
     cy.intercept('https://pip-player.zum.com/**', {statusCode: 200});
+  });
 
+  const visit = () => {
     cy.visit('/');
     cy.get('.gnb_finance')
       .find('a')
@@ -14,9 +19,11 @@ describe('투자노트', () => {
       .click();
 
     cy.wait('@apiInvestment');
-  });
+  };
 
   describe('투자노트 TOP6', () => {
+    beforeEach(visit);
+
     it('카드형태로 보여준다.', () => {
       cy.withHidden('#header', () => {
         cy.get('.invest_note_list')
@@ -54,39 +61,38 @@ describe('투자노트', () => {
 
   describe('최신글', () => {
     beforeEach(() => {
+      visit();
+
       cy.contains('최신글').click();
       cy.wait('@posts');
     });
 
     it('최신글에서 카테고리 선택을 할 수 있다.', () => {
-      const categoryTable = {
-        '전체': 'all',
-        '국내증시': 'domesticStock',
-        '해외증시': 'overseasStock',
-        '가상화폐': 'coin',
-        '대체투자': 'alternativeInvestment',
-        '투자트렌드': 'investmentTrend',
-      };
       cy.get('.lasted_write_wrap')
         .within(() => {
           cy.get('ul.menu_tab > li > a')
             .reverse()
-            .each($category => {
-              const categoryText = $category.text();
-              cy.contains(categoryText)
-                .click({force: true})
+            .clickEachWithTable(
+              {
+                '전체': 'all',
+                '국내증시': 'domesticStock',
+                '해외증시': 'overseasStock',
+                '가상화폐': 'coin',
+                '대체투자': 'alternativeInvestment',
+                '투자트렌드': 'investmentTrend',
+              },
+              id => cy
                 .wait('@posts')
                 .its('request.url')
-                .should('contain', `category=${categoryTable[categoryText]}`);
-
-              cy.wrap($category).should('be.activated');
-            });
+                .should('contain', `category=${id}`),
+            );
         });
     });
   });  // END: 최신글
 
   describe('줌 투자 필진', () => {
     beforeEach(() => {
+      visit();
 
       cy.get('.writers_wrap')
         .scrollIntoView()
@@ -137,32 +143,61 @@ describe('투자노트', () => {
 
     // TODO: 테스트 정렬 (가독성)
     // FIXME: 다음버튼이 요청을 두번 보내는 문제 존재
-    it.skip('이전/다음 버튼을 클릭하여 정보를 볼 수 있다', () => {
-      cy.get('@writersWrap').within(() => {
-        const [, _count, total] = Cypress
-          .$('.writers_wrap .count')
-          .text()
-          .match(/(\d+)[^\d](\d+)/)
-          .map(d => parseInt(d, 10));
+    it('이전/다음 버튼을 클릭하여 정보를 볼 수 있다', () => {
+      const waitUntil = (alias, predicate, options) =>
+        recurse(
+          () => cy.get(alias),
+          http => http && predicate(http),
+          {
+            ...options,
+            limit: Infinity,
+            timeout: Cypress.config('requestTimeout') || 5000,
+            delay: 1,
+            log: false,
+          },
+        );
+      
+      const waitForAuthorsApiUntil = predicate =>
+        waitUntil('@apiAuthors', predicate, { delay: 100, limit: 3 });
 
-        cy.get('.next')
-          .click()
-          .wait('@apiAuthors')
-          .its('request.url')
-          .should('contain', 'page=2');
+      cy.get('@writersWrap')
+        .within(() => {
+          cy.get('.next')
+            .click();
 
-        cy.get('.prev')
-          .click()
-          .wait('@apiAuthors')
-          .its('request.url')
-          .should('contain', 'page=1');
+          waitForAuthorsApiUntil(
+            ({ request }) => expect(request.url).to.contain('page=2'),
+          );
 
-        cy.get('.prev')
-          .click()
-          .wait('@apiAuthors')
-          .its('request.url')
-          .should('contain', `page=${total}`);
-      });
+          cy.get('.prev')
+            .click();
+
+          waitForAuthorsApiUntil(
+            ({ request }) => expect(request.url).to.contain('page=1'),
+          );
+
+          cy.get('.count')
+            .invoke('text')
+            .then(text => {
+              const [, _count, total] = text
+                .match(/(\d+)[^\d](\d+)/)
+                .map(d => parseInt(d, 10));
+
+              cy.get('.prev')
+                .click();
+
+              waitForAuthorsApiUntil(
+                ({ request }) => expect(request.url).to.contain(`page=${total}`),
+              );
+            })
+
+          cy.get('.next')
+            .click();
+
+          waitForAuthorsApiUntil(
+            ({ request }) => expect(request.url).to.contain('page=1'),
+          );
+        });
     });
   });  // END: 줌 투자 필진
 
