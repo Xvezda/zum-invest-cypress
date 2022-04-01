f44869420107a1e2ada1c134cec95f4cdb51ab95
Xvezda <xvezda@naver.com>
2022-04-01 16:01:36 +0900

    refactor: 섬네일 stub 파일 분리

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,46 +14,16 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
+import './thumbnails';
+
 beforeEach(() => {
   cy.fixCypressSpec();
 
-  cy.intercept(/^https?:\/\/thumb\.zumst\.com\/.*/, req => {
-    const url = new URL(req.url);
-    if (url.pathname.startsWith('/378x241')) {
-      req.reply({fixture: '378x241.jpg'});
-    } else if (url.pathname.startsWith('/182x77')) {
-      req.reply({fixture: '182x77.jpg'});
-    } else if (url.pathname.startsWith('/166x116')) {
-      req.reply({fixture: '166x116.jpg'});
-    } else if (url.pathname.startsWith('/76x48')) {
-      req.reply({fixture: '76x48.jpg'});
-    } else if (url.pathname.startsWith('/54x34')) {
-      req.reply({fixture: '54x34.jpg'});
-    } else {
-      req.reply({fixture: 'fallback.jpg'});
-    }
-  });
-
-  cy.intercept('https://pip-thumb.zumst.com/api/v1/**', req => {
-    const url = new URL(req.url);
-    const width = url.searchParams.get('w');
-    const height = url.searchParams.get('h');
-    if (width === '880' && height === '495') {
-      req.reply({fixture: '880x495.jpg'});
-    } else {
-      req.reply({fixture: '640x360.jpg'});
-    }
-  });
-
-  cy.intercept(/^https?:\/\/finance\.zumst\.com\/writing\/.+\.(png|jpe?g|gif)$/, {
-    fixture: 'writer.png'
-  });
-  cy.intercept('https://static.news.zumst.com/images/**', {fixture: '640x360.jpg'});
   cy.intercept('https://finance.zumst.com/content/**', {statusCode: 200});
 
   cy.intercept('https://zvod.zumst.com/zumvrix/zvod/**', {statusCode: 206});
