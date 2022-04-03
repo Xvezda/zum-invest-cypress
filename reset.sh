0f5106590e998c871ff4a0c028c92d36a395c2b9
Xvezda <xvezda@naver.com>
2022-04-04 08:44:45 +0900

    chore: 파일의 존재여부 확인 추가
    존재하지 않는 파일의 생성을 방지

--- reset.sh
+++ reset.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-	docker exec e2e touch /e2e/cypress/integration/$1_spec.js
+	[ -e /e2e/cypress/integration/$1_spec.js ] && \
+		docker exec e2e touch /e2e/cypress/integration/$1_spec.js
 else
 	docker exec e2e touch /e2e/cypress.json
 fi
