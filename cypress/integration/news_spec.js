1eeef09dc2136c0231d5afbefc5140624ef42f4f
Xvezda <xvezda@naver.com>
2022-04-11 10:45:47 +0900

    chore: 주석 추가

--- cypress/integration/news_spec.js
+++ cypress/integration/news_spec.js
@@ -3,7 +3,9 @@ describe('줌 투자 뉴스', () => {
     cy.request('/news/article/3333333')
       .its('body')
       .then(html => {
+        // 이후의 스크립트 실행을 방지하기 위해 번들링 된 파일의 경로를 차단
         cy.intercept('/static/js/**', {statusCode: 503});
+        // 빈 페이지에 HTML 값을 추가
         cy.document()
           .invoke({log: false}, 'write', html)
       });
@@ -12,4 +14,4 @@ describe('줌 투자 뉴스', () => {
       .its('body')
       .should('contain', '대한민국이 승리한다');
   });
-});
+});  // END: 줌 투자 뉴스
