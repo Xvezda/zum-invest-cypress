e85507384e4be358baf393256197fdcc5844bd8b
Xvezda <xvezda@naver.com>
2022-04-11 09:07:04 +0900

    chore: 잘못된 파일 확인 명령 수정

--- reset.sh
+++ reset.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ $# -eq 1 ]; then
-	[ -e /e2e/cypress/integration/$1_spec.js ] && \
-		docker exec e2e touch /e2e/cypress/integration/$1_spec.js
+PROJECT_ROOT=$(git rev-parse --show-toplevel)
+if [ $# -eq 1 ] && [ -e $PROJECT_ROOT/cypress/integration/$1_spec.js ]; then
+	docker exec e2e touch /e2e/cypress/integration/$1_spec.js
 else
 	docker exec e2e touch /e2e/cypress.json
 fi
